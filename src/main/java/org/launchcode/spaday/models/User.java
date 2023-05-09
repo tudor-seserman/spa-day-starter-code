088e5f4b55d924b37bd4a280f966228089b17b6d
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2023-05-08 19:14:06 -0500

    Studio 15 done

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,22 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotNull
+    @NotBlank
     private String username;
+    @Email
     private String email;
+    @NotNull
+    @NotBlank
+    @Size(min=6)
     private String password;
+    @NotNull(message = "Passwords do not match")
+    private String verifyPassword;
 
     public User() {
 
@@ -14,6 +27,12 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    private void checkPassword(){
+        if(this.password != null && this.verifyPassword != null && !verifyPassword.equals(password)) {
+            verifyPassword = null;
+        }
     }
 
     public String getUsername() {
@@ -38,5 +57,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 }
